b9377bf03a1cba90c80aaea30fabc157e3723a61
Morau Fiodar <fiodar.morau@sharekey.com>
2019-10-15 17:12:08 +0300

    repro: issue with image rendering gif

--- App.js
+++ App.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-
-import LottieView from 'lottie-react-native';
+import {StyleSheet, Image, View} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => (
-  <LottieView 
-    style={styles.lottie}
-    source={require('./anim.json')}
-    autoPlay
-    loop
-  />
+  <View style={styles.app}>
+    <Image style={styles.img} source={require('./spinner.gif')} />
+  </View>
 );
 
 const styles = StyleSheet.create({
   app: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: Colors.lighter,
   },
   lottie: {
     flex: 1,
   },
+  img: {
+    width: 30,
+    height: 30,
+},
 });
 
 export default App;
